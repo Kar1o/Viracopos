860e5835762f1562d1f333b576f101592e22a543
kar1o <carlos.diego.fig@gmail.com>
2015-04-19 15:15:27 -0300

    Initial commit

---
+++ src/sample/ControllerNome.java
@@ -0,0 +1,101 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Created by k on 4/18/15.
+ */
+public class ControllerNome implements Initializable{
+
+    @FXML
+    private TextArea player1;
+
+    @FXML
+    private TextArea player2;
+
+    @FXML
+    private TextArea player3;
+
+    @FXML
+    private TextArea player4;
+
+    @FXML
+    private Label lblPlayer3;
+
+    @FXML
+    private Label lblPlayer4;
+
+    @FXML
+    private Button btnStart;
+
+    public static List<String> parameters = new ArrayList<String>();
+
+
+    @Override
+    public void initialize(URL url, final ResourceBundle resourceBundle) {
+
+        System.out.println(ControllerJogador.parameters);
+
+        if (ControllerJogador.parameters.equals("2")){
+            lblPlayer3.setVisible(false);
+            player3.setVisible(false);
+            lblPlayer4.setVisible(false);
+            player4.setVisible(false);
+        }
+        else if (ControllerJogador.parameters.equals("3")){
+            lblPlayer4.setVisible(false);
+            player4.setVisible(false);
+        }
+
+        btnStart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                try {
+                    //popular array com valores de nomes
+                    parameters.add(player1.getText());
+                    parameters.add(player2.getText());
+                    parameters.add(player3.getText());
+                    parameters.add(player4.getText());
+
+                    Parent root;
+                    try {
+                        root = FXMLLoader.load(getClass().getClassLoader().getResource("sample/quiz.fxml"), resourceBundle);
+                        Stage stage = new Stage();
+                        stage.setTitle("Viracopos");
+                        stage.setScene(new Scene(root, 800, 600));
+                        stage.show();
+
+                        //esconder janela atual
+                        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("qtd_jogador.fxml"));
+        stage.setTitle("Viracopos");
+        stage.setScene(new Scene(root, 600, 450));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
